9663329dfb0fb2ac545c165bee2c95f7e289ee2e
Joshua_Musyoki <musyokijoxhua@gmail.com>
2023-10-07 19:01:35 +0300

    Working on the Sidebar

---
+++ frontend/src/pages/global/Layout.js
@@ -0,0 +1,19 @@
+// import { Component } from "react";
+import SidebarAdm from './Sidebar';
+import { Box } from "@mui/material";
+const Layout=(Component)=>(({...props})=>{
+    return(
+        <>
+        <div style={{display:'flex', minHeight:'100vh'}}>
+            <SidebarAdm />
+            <Box sx={{width:'100%',  bgcolor:'#002952'}}>
+                <HeaderTop />
+                <Box sx={{p:3}}>
+                    <Component  {...props} />
+                </Box>
+            </Box>
+        </div>
+        </>
+    )
+})
+export default Layout
