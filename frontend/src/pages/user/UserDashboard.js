a0626f74d75b01643574c30be562135ecceaf3b9
Joshua_Musyoki <musyokijoxhua@gmail.com>
2023-10-10 12:12:27 +0300

    Worked on User Profile and added Stats Component

--- frontend/src/pages/user/UserDashboard.js
+++ frontend/src/pages/user/UserDashboard.js
@@ -1,8 +1,41 @@
+import { Box, Stack, Typography } from '@mui/material'
 import React from 'react'
+import StatComponent from '../../components/StatComponent'
+// import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount'
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth'
+import WorkIcon from '@mui/icons-material/Work'
+import { useSelector } from 'react-redux'
 
 const UserDashboard = () => {
+  const {user}=useSelector(state=>state.userProfile)
   return (
-    <div>UserDashboard</div>
+    <>
+    <Box>
+      <Typography variant='h4' sx={{color:'white', pb: 3}}>
+        Dashboard
+      </Typography>
+      <Stack
+      direction={{xs:'column', sm:'row'}}
+      spacing={{xs:1, sm:2, md:4}}
+      >
+
+      <StatComponent
+      value={user && user.createAt}
+      icon={<CalendarMonthIcon sx={{color:"#fafafa", fontSize:30}}/>}
+      description="Member Since"
+      money=''
+      />
+
+      <StatComponent
+      value={user && user.jobsHistory.length}
+      icon={<WorkIcon sx={{color:"#fafafa", fontSize:30}}/>}
+      description="Jobs Submitted"
+      money=''
+      />
+
+      </Stack>
+    </Box>
+    </>
   )
 }
 
