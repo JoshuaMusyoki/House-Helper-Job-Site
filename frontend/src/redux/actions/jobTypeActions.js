9959d3ee4bf1c653a6ed7341748dac704664cd3e
Joshua_Musyoki <musyokijoxhua@gmail.com>
2023-11-01 17:57:24 +0300

    Added DashCreateCategory

--- frontend/src/redux/actions/jobTypeActions.js
+++ frontend/src/redux/actions/jobTypeActions.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { JOB_TYPE_LOAD_FAIL, JOB_TYPE_LOAD_REQUEST, JOB_TYPE_LOAD_SUCCESS } from '../constants/jobTypeConstant';
+import { CREATE_JOB_TYPE_FAIL, CREATE_JOB_TYPE_REQUEST, CREATE_JOB_TYPE_SUCCESS, JOB_TYPE_LOAD_FAIL, JOB_TYPE_LOAD_REQUEST, JOB_TYPE_LOAD_SUCCESS } from '../constants/jobTypeConstant';
 
 export const jobTypeLoadAction=()=>async(dispatch)=>{
     dispatch({type:JOB_TYPE_LOAD_REQUEST});
@@ -14,5 +14,27 @@ export const jobTypeLoadAction=()=>async(dispatch)=>{
             type:JOB_TYPE_LOAD_FAIL,
             payload:error.response.data.error
         });
+    }
+}
+
+export const createJobTypeAction=()=>async(dispatch)=>{
+    dispatch({ type: CREATE_JOB_TYPE_REQUEST })
+
+    try {
+        const { data } = await axios.post("/api/type/create", jobtype)
+        dispatch({
+            type: CREATE_JOB_TYPE_SUCCESS,
+            payload: data
+        })
+        toast.success("Job type created successfully");
+
+
+    } catch (error) {
+        dispatch({
+            type: CREATE_JOB_TYPE_FAIL,
+            payload: error.response.data.error
+        })
+        toast.error(error.response.data.error);
+
     }
 }
