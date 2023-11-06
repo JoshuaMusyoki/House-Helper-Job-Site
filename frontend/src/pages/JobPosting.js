88b9f55b12fbe7a190d6e98ff9d7a6308b122ae9
Joshua_Musyoki <musyokijoxhua@gmail.com>
2023-11-06 16:57:33 +0300

    Styled Job Posting form and some endpoint fixing

--- frontend/src/pages/JobPosting.js
+++ frontend/src/pages/JobPosting.js
@@ -1,5 +1,9 @@
+import { Box, Button, TextField } from "@mui/material";
 import axios from "axios";
 import React, { useState } from "react";
+import { useFormik } from "formik";
+import { registerAjobAction } from "../redux/actions/jobActions";
+import { useDispatch } from "react-redux";
 
 const JobPostForm =()=>{
     const [jobTitle, setJobTitle] = useState("")
@@ -7,34 +11,66 @@ const JobPostForm =()=>{
     const [jobLocation, setJobLocation] = useState("")
     const [jobSalary, setJobSalary] = useState(null)
     const [jobType, setJobType] = useState(null)
+    const dispatch = useDispatch();
 
     const postJob = (e) => {
         const data = { title: jobTitle, description: jobDescription, location: jobLocation,
                         salary: jobSalary, jtype:jobType }
-        axios.post('/api/job/create', data)
+        // axios.post('/api/job/create', data)
+        dispatch(registerAjobAction(data));
+        e.resetForm()
         .then(response => {
           console.log(response)
         })
     }
+
+    // const formik=useFormik({
+    //     initialValues:{
+    //         title:'',
+    //         description:'',
+    //         location:'',
+    //         salary:'',
+    //         jobType:''
+    //     },
+    //     onSubmit: (values, actions) => {
+    //         // alert(JSON.stringify(values, null, 2));
+    //       dispatch(registerAjobAction(values));
+    //       actions.resetForm();
+    //     }
+   // })
     return(
         
         <>
         <div className="submitJobContainer">
             <h3>Submit Job</h3>
-            <form className="formContainer" onSubmit={postJob}>
-                <input type="text" name="title" placeholder="Job Title" 
+            <Box onSubmit={postJob} component="form" className='form_style border-style' >
+                    <Box sx={{ md:3, display: "flex", flexDirection: "column", alignItems: "center", width: "100%" }}>
+            <form method='POST' action="submit-job" className="formContainer" onSubmit={postJob}>
+                <TextField sx={{md:3, padding:1}}
+                fullWidth
+                 type="text" name="title" placeholder="Job Title" 
                   onChange={e => setJobTitle(e.target.value)} />
-                <input type="text" name="description" placeholder="Job Description"
+                <TextField sx={{md:3, padding:1}}
+                fullWidth
+                 type="text" name="description" placeholder="Job Description"
                     onChange={e => setJobDescription(e.target.value)} />
-                <input type="text" name="location" placeholder="Job Location"
+                <TextField sx={{md:3, padding:1}}
+                fullWidth
+                 type="text" name="location" placeholder="Job Location"
                     onChange={e => setJobLocation(e.target.value)} />
-                <input type="number" name="salary" placeholder="Job Salary"
+                <TextField sx={{md:3, padding:1}}
+                fullWidth 
+                type="number" name="salary" placeholder="Job Salary"
                     onChange={e => setJobSalary(e.target.value)} />
 
-                    <input type="text" name="jtype" placeholder="Job Type e.g House Help, Electrician etc"
+                    <TextField sx={{md:3, padding:1}}
+                     fullWidth 
+                     type="text" name="jtype" placeholder="Job Type e.g House Help, Electrician etc"
                     onChange={e => setJobType(e.target.value)} />
-                <button className="submitButton" type="submit">Submit</button>
+                <Button className="submitButton" type="submit">Submit</Button>
             </form>
+            </Box>
+            </Box>
         </div>
         </>
     )
--- frontend/src/pages/SignUp/UserRegister.js
+++ frontend/src/pages/SignUp/UserRegister.js
@@ -51,10 +51,7 @@ const UserRegister = () => {
         }
         
       };
-      const passwordsMatch = profileData.password === profileData.confirmPassword;
-     
-    
-    
+      const passwordsMatch = profileData.password === profileData.confirmPassword;    
     const handleSubmit = (event, values, actions) => {
         event.preventDefault();
         dispatch(userSignUpAction(values))
@@ -122,7 +119,7 @@ const UserRegister = () => {
             <div className="d-flex justify-content-center align-items-center text-center vh-100" style= {{backgroundImage : "linear-gradient(#00d5ff,#0095ff,rgba(93,0,255,.555))"}}>
                 <div className="bg-white p-3 rounded" style={{width : '100%'}}>
                     <h2 className='mb-3 text-primary'>Register</h2>
-                    <form onSubmit={handleSubmit}>
+                    <form method='POST' onSubmit={handleSubmit} action='register'>
                         <div className="mb-3 text-start">
                             <label htmlFor="exampleInputEmail1" className="form-label">
                                 <strong >First Name</strong>
